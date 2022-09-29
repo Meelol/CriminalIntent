f9035845df1f18254ac7e0222a8fb5c45dfaea9d
Omar Rodriguez <eomarrodriguez@outlook.com>
2022-09-29 11:25:50 -0700

    Initial commit

---
+++ app/src/main/java/com/omar/criminalintent/Crime.kt
@@ -0,0 +1,7 @@
+package com.omar.criminalintent
+
+import java.util.*
+
+data class Crime(val id: UUID = UUID.randomUUID(),
+                 var title: String ="", var date: Date = Date(),
+                 var isSolved: Boolean = false)
